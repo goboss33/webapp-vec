8bbb2e501d0f5aa1957f165d96e4f27c59806097
goboss33 <goboss33@gmail.com>
2025-05-20 16:12:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@ console.log('config.js module loaded');
 
 export const N8N_GET_DATA_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/get-product-data';
 export const N8N_UPDATE_DATA_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/update-product';
-
 export const N8N_CROP_IMAGE_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/crop-n-replace-img';
 export const N8N_REMOVE_WATERMARK_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/remove-watermark';
 export const N8N_GENERATE_MOCKUP_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/create-mockup';
-
 export const N8N_REPLACE_BACKGROUND_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/replace-background';
+
+export const N8N_UPSCALE_IMAGE_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/remove-watermark';
