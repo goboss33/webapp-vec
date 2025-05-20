c2d7a93f0684d1c2327347a96ad93406c6b69ab4
goboss33 <goboss33@gmail.com>
2025-05-20 16:15:04 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,4 +9,4 @@ export const N8N_REMOVE_WATERMARK_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webh
 export const N8N_GENERATE_MOCKUP_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/create-mockup';
 export const N8N_REPLACE_BACKGROUND_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/replace-background';
 
-export const N8N_UPSCALE_IMAGE_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/remove-watermark';
+export const N8N_UPSCALE_IMAGE_WEBHOOK_URL = 'https://n8n.scalableweb.ch/webhook/webapp/upscale-image';
